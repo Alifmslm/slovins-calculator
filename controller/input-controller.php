e9cdb58b8c0e67d77233bc73c95b5106d462139a
Alifmslm <alifmslm01@gmail.com>
2025-09-27 16:40:01 +0700

    ADD: Slovins Method Calculation

--- controller/input-controller.php
+++ controller/input-controller.php
@@ -4,16 +4,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $population = filter_input(INPUT_POST,'input_population', FILTER_VALIDATE_INT);
     $error_rate = filter_input(INPUT_POST,'input_error', FILTER_VALIDATE_FLOAT);
 
+    // ubah bentuk angka menjadi persen(desimal)
+    $error_percen = $error_rate / 100;
+
+    $result = null;
+
     if ($population === false || $error_rate === false) {
         echo'Tipe data anda salah';
     } else if ($population <= 0 || $error_rate <= 0){
         echo 'Input tidak boleh 0';
     } else {
-        echo'Pouplation: ' . $population . ", Tipe Data: " . gettype($population);
-        echo'<br>';
-        echo'Error Rate: ' . $error_rate . ", Tipe Data: " . gettype($error_rate);
+        $denominator = 1 + $population * pow($error_percen, 2);
+        
+        try {
+            $result = $population / $denominator;
+        } catch (DivisionByZeroError $ez) {
+            echo "Terjadi Error Pembagian dengan Nol: " . $e->getMessage();
+        } catch (Exception $e) {
+            echo "Error ditemukan!: ". $e->getMessage();
+        }
+
+        $rounded_result = round($result);
+
+        echo$rounded_result;
     }
-} else {
-    echo "Gagal Melakukan Proses Post.";
 }
 ?>
