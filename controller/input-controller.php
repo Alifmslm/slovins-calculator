835098c1ce5ef0f0b0fbd7115fd3af2b81b5d456
Alifmslm <alifmslm01@gmail.com>
2025-09-27 10:56:13 +0700

    FIX: Input Validation

--- controller/input-controller.php
+++ controller/input-controller.php
@@ -1,14 +1,18 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     # code...
-    $population = htmlspecialchars($_POST["input_population"]);
-    $error = htmlspecialchars($_POST["input_error"]);
+    $population = filter_input(INPUT_POST,'input_population', FILTER_VALIDATE_INT);
+    $error_rate = filter_input(INPUT_POST,'input_error', FILTER_VALIDATE_FLOAT);
 
-    $int_population = (int)$population;
-    $int_error = (int)$error / 100;
-
-    $result = round($int_population / (1 + $int_population * pow(($int_error), 2)));
-    echo $result;
+    if ($population === false || $error_rate === false) {
+        echo'Tipe data anda salah';
+    } else if ($population <= 0 || $error_rate <= 0){
+        echo 'Input tidak boleh 0';
+    } else {
+        echo'Pouplation: ' . $population . ", Tipe Data: " . gettype($population);
+        echo'<br>';
+        echo'Error Rate: ' . $error_rate . ", Tipe Data: " . gettype($error_rate);
+    }
 } else {
     echo "Gagal Melakukan Proses Post.";
 }
