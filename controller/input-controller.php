b43b86491bd010953de693260764d7dd9e5fc6e5
Alifmslm <alifmslm01@gmail.com>
2025-09-28 11:19:29 +0700

    FIX: Data Type Popultion Error

--- controller/input-controller.php
+++ controller/input-controller.php
@@ -1,7 +1,11 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     # code...
-    $population = filter_input(INPUT_POST,'input_population', FILTER_VALIDATE_INT);
+    $population_raw = $_POST["input_population"];
+
+    $population_clean = str_replace(",","", $population_raw);
+
+    $population = filter_var($population_clean, FILTER_VALIDATE_INT);
     $error_rate = filter_input(INPUT_POST,'input_error', FILTER_VALIDATE_FLOAT);
 
     // ubah bentuk angka menjadi persen(desimal)
@@ -11,6 +15,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($population === false || $error_rate === false) {
         echo'Tipe data anda salah';
+        echo'<br>';
+        echo'Tipe Data Populasi: ' . gettype($population);
+        echo'<br>';
+        echo'Tipe Data Error: ' . gettype($error_rate);
     } else if ($population <= 0 || $error_rate <= 0){
         echo 'Input tidak boleh 0';
     } else {
