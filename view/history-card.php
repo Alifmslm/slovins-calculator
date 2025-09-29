df26bf665c460bc8e7ffdbf0b6d148cd94a9eedb
Alifmslm <alifmslm01@gmail.com>
2025-09-29 10:57:55 +0700

    ADD: History UI (non responsive)

--- view/history-card.php
+++ view/history-card.php
@@ -1,1 +1,32 @@
-<p>Card History</p>
+<div class="card">
+    <div class="header-card d-flex flex-column gap-2 p-3">
+        <div class="item-text d-flex gap-1">
+            <p class="card-subtitle">Jumlah Populasi: </p>
+            <p class="card-subtitle"><strong>100</strong></p>
+        </div>
+        <div class="item-text d-flex gap-1">
+            <p class="card-subtitle">Persentase Error: </p>
+            <p class="card-subtitle"><strong>5%</strong></p>
+        </div>
+        <h5 class="card-title mt-2 fw-bold">365 Sampel Dibutuhkan</h5>
+    </div>
+    <div class="card-footer d-flex justify-content-end">
+        <button class="btn btn-danger">Hapus History</button>
+    </div>
+</div>
+<div class="card">
+    <div class="header-card d-flex flex-column gap-2 p-3">
+        <div class="item-text d-flex gap-1">
+            <p class="card-subtitle">Jumlah Populasi: </p>
+            <p class="card-subtitle"><strong>100</strong></p>
+        </div>
+        <div class="item-text d-flex gap-1">
+            <p class="card-subtitle">Persentase Error: </p>
+            <p class="card-subtitle"><strong>5%</strong></p>
+        </div>
+        <h5 class="card-title mt-2 fw-bold">365 Sampel Dibutuhkan</h5>
+    </div>
+    <div class="card-footer d-flex justify-content-end">
+        <button class="btn btn-danger">Hapus History</button>
+    </div>
+</div>
--- view/history.php
+++ view/history.php
@@ -1,6 +1,8 @@
-<div class="container">
-    <h3 class="fw-bold">Halaman History</h3>
-    <?php
-        include_once("history-card.php");
-    ?>
+<div class="container w-50 mb-5">
+    <h3 class="fw-bold mb-4">List History 📜</h3>
+    <div class="d-flex flex-column gap-3">
+        <?php
+            include("history-card.php");
+        ?>
+    </div>
 </div>
