50cee169267aebb6aebd399477917b04880cdd2f
Alifmslm <alifmslm01@gmail.com>
2025-09-29 11:27:51 +0700

    ADD: Setup DB

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    $databaseHost = 'localhost';
+    $databaseName = 'slovins-calculator';
+    $databaseUsername = 'root';
+    $databasePassword = '';
+
+    $mysqli = mysqli_connect($databaseHost, $databaseName, $databaseUsername, $databasePassword);
+?>
