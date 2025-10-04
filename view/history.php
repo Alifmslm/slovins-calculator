1003a371ebf54afdbd2f853b4a753fb9de27c6be
Alifmslm <alifmslm01@gmail.com>
2025-10-04 20:23:01 +0700

    ADD: Sticky Position in History Card List

--- view/history.php
+++ view/history.php
@@ -1,6 +1,6 @@
 <div class="container w-50 mb-5">
     <h3 class="fw-bold mb-4">List History 📜</h3>
-    <div class="d-flex flex-column gap-3">
+    <div class="d-flex flex-column gap-3 overflow-auto" style="height: 300px">
         <?php
             include("history-card.php");
         ?>
