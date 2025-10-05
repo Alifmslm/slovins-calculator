b4a81ea0e496b622345bcdff0932faf79bf92eea
Alifmslm <alifmslm01@gmail.com>
2025-10-05 23:29:52 +0700

    FEATURE: Add Validation Modal when Deleting History

--- view/history-card.php
+++ view/history-card.php
@@ -18,7 +18,28 @@ if ($list_data_array === null) {
                 <h5 class="card-title mt-2 fw-bold history-value"><?= number_format($row['result']) ?> Sampel Dibutuhkan</h5>
             </div>
             <div class="card-footer d-flex justify-content-end">
-                <a href="index.php?id=<?= $row['id'] ?>" class="btn btn-danger">Hapus History</a>
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Hapus History
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Menghapus Data History</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                Apakah anda yakin ingin menghapus data history ini?
+                            </div>
+                            <div class="modal-footer">
+                                <a href="index.php?id=<?= $row['id'] ?>" type="button" class="btn btn-danger">Ya, Hapus History</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <?php
